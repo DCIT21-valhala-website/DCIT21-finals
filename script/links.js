aab8b3420caed7f47c07321b27d87539b84c5a4a
Rolando Anacta <dianacta23@gmail.com>
2025-01-23 22:39:45 +0800

    Enhance dropdown interaction and styling in navigation links

--- script/links.js
+++ script/links.js
@@ -1,12 +1,33 @@
 const links = document.getElementsByClassName("header-links");
+const dropdown = document.getElementsByClassName("dropdown-content");
+const linksContainer = document.getElementsByClassName("links");
 
 if (links) {
   for (let i = 0; i < links.length; i++) {
     links[i].addEventListener("mouseover", () => {
-      links[i].classList.add("active");
+      for (let j = 0; j < links.length; j++) {
+        if (j !== i) {
+          links[j].style.color = "#acacac";
+        }
+      }
     });
     links[i].addEventListener("mouseout", () => {
-      links[i].classList.remove("active");
+      for (let j = 0; j < links.length; j++) {
+        if (j !== i) {
+          links[j].style.color = "#000";
+        }
+      }
+    });
+  }
+}
+
+if (linksContainer) {
+  for (let i = 0; i < linksContainer.length; i++) {
+    linksContainer[i].addEventListener("mouseover", () => {
+      dropdown[0].classList.add("dropdown-content-hover");
+    });
+    linksContainer[i].addEventListener("mouseout", () => {
+      dropdown[0].classList.remove("dropdown-content-hover");
     });
   }
 }
