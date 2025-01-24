a36d222aabb1d50fdbd811a7bf185758b0e7facb
Rolando Anacta <dianacta23@gmail.com>
2025-01-24 15:20:10 +0800

    Enhance dropdown functionality and styling; add "New & Featured" link and improve hover effects

--- script/links.js
+++ script/links.js
@@ -1,8 +1,14 @@
 const links = document.getElementsByClassName("header-links");
-const dropdown = document.getElementsByClassName("dropdown-content");
+const dropdown = document.getElementById("dropdown-content");
 const linksContainer = document.getElementsByClassName("links");
 
+const featuresH1 = document.createElement("h1");
+const featuresH1Text = document.createTextNode("Featured");
+featuresH1.appendChild(featuresH1Text);
+
+// * Add event listener to links
 if (links) {
+  // * Makes the other links grey when hovering over one
   for (let i = 0; i < links.length; i++) {
     links[i].addEventListener("mouseover", () => {
       for (let j = 0; j < links.length; j++) {
@@ -21,13 +27,74 @@ if (links) {
   }
 }
 
+// * Add event listener to dropdown
 if (linksContainer) {
-  for (let i = 0; i < linksContainer.length; i++) {
-    linksContainer[i].addEventListener("mouseover", () => {
-      dropdown[0].classList.add("dropdown-content-hover");
-    });
-    linksContainer[i].addEventListener("mouseout", () => {
-      dropdown[0].classList.remove("dropdown-content-hover");
-    });
-  }
+  // * Makes the dropdown stays open when hovering over it
+  dropdown.addEventListener("mouseover", () => {
+    dropdown.style.height = "20rem";
+    dropdown.style.transition = "all 100ms ease";
+  });
+  dropdown.addEventListener("mouseout", () => {
+    dropdown.style.transition = "none";
+    dropdown.style.transform = "translateY(0)";
+    dropdown.style.height = "0";
+  });
+
+  // * Makes the dropdown appear when hovering over the "New and Featured" link
+  linksContainer[0].addEventListener("mouseover", () => {
+    dropdown.style.height = "20rem";
+    dropdown.style.transition = "all 100ms ease";
+
+    // * Add content to dropdown
+    
+  });
+  linksContainer[0].addEventListener("mouseout", () => {
+    dropdown.style.transition = "none";
+    dropdown.style.transform = "translateY(0)";
+    dropdown.style.height = "0";
+  });
+
+  // * Makes the dropdown appear when hovering over the "Men" link
+  linksContainer[1].addEventListener("mouseover", () => {
+    dropdown.style.height = "20rem";
+    dropdown.style.transition = "all 100ms ease";
+  });
+  linksContainer[1].addEventListener("mouseout", () => {
+    dropdown.style.transition = "none";
+    dropdown.style.transform = "translateY(0)";
+    dropdown.style.height = "0";
+  });
+
+  // * Makes the dropdown appear when hovering over the "Women" link
+  linksContainer[2].addEventListener("mouseover", () => {
+    dropdown.style.height = "20rem";
+    dropdown.style.transition = "all 100ms ease";
+  });
+  linksContainer[2].addEventListener("mouseout", () => {
+    dropdown.style.transition = "none";
+    dropdown.style.transform = "translateY(0)";
+    dropdown.style.height = "0";
+  });
+
+  // * Makes the dropdown appear when hovering over the "Kids" link
+  linksContainer[3].addEventListener("mouseover", () => {
+    dropdown.style.height = "20rem";
+    dropdown.style.transition = "all 100ms ease";
+  });
+  linksContainer[3].addEventListener("mouseout", () => {
+    dropdown.style.transition = "none";
+    dropdown.style.transform = "translateY(0)";
+    dropdown.style.height = "0";
+  });
+
+  // * Makes the dropdown appear when hovering over the "Sale" link
+  linksContainer[4].addEventListener("mouseover", () => {
+    dropdown.style.height = "20rem";
+    dropdown.style.transition = "all 100ms ease";
+  });
+  linksContainer[4].addEventListener("mouseout", () => {
+    dropdown.style.transition = "none";
+    dropdown.style.transform = "translateY(0)";
+    dropdown.style.height = "0";
+  });
 }
